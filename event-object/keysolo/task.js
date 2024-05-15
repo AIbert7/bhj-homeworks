6eb0c7e85c83336a12f64d0ce79515ee02a98410
Albert <ananyan-albert@mail.ru>
2024-05-15 21:18:00 +0300

    FrstHw

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,14 +17,12 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода символа вызываем this.success()
-      При неправильном вводе символа - this.fail();
-      DOM-элемент текущего символа находится в свойстве this.currentSymbol.
-     */
+
+    document.addEventListener('keyup', ev => {
+
+      ev.key.toLowerCase() == this.currentSymbol.textContent ? this.success() : this.fail();
+      });
+      
   }
 
   success() {
